564c087b071a344fd401d41809f4db683bf94e88
Gennadii Plahonin <plahonin99@mail.ru>
2022-08-31 10:01:02 +0900

    Removed unused imports

--- src/main/java/ru/kata/spring/boot_security/demo/dao/UserDaoImpl.java
+++ src/main/java/ru/kata/spring/boot_security/demo/dao/UserDaoImpl.java
@@ -1,15 +1,13 @@
 package ru.kata.spring.boot_security.demo.dao;
 
 import org.springframework.stereotype.Repository;
-import ru.kata.spring.boot_security.demo.model.Role;
 import ru.kata.spring.boot_security.demo.model.User;
-
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
+
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 
 
 @Repository
